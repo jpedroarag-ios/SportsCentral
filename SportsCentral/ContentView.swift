982efcc812ffa8753f2048530e193d033bf58507
Rodrigo Silva Ribeiro <Rodrigo.ato@hotmail.com>
2022-08-08 18:47:04 -0300

    Initial Commit

---
+++ SportsCentral/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SportsCentral
+//
+//  Created by Rodrigo Silva Ribeiro on 08/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SportsCentral/SportsCentralApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportsCentralApp.swift
+//  SportsCentral
+//
+//  Created by Rodrigo Silva Ribeiro on 08/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SportsCentralApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
